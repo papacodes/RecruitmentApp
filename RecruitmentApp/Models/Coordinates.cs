eac22a1281f2cf90c5e319e0887ff18553d0ad2d
Papa <mabotjapapa@gmail.com>
2022-05-16 21:57:23 +0200

    init commit due to load shedding at 10

---
+++ RecruitmentApp/Models/Coordinates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentApp.Models
+{
+    public class Coordinates
+    {
+
+        public Int32 PositionId { get; set; }
+        public string? VehicleRegistration { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public Int64? RecordedTimeUTC { get; set; }
+
+
+    }
+}
---
+++ RecruitmentApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RecruitmentApp.Models;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public List<Coordinates>? Coordinates { get; set; }
+
+        static void Main(string[] args)
+        {
+            List<Coordinates> coordinates = new List<Coordinates>();
+
+            ReadBinaryFile(coordinates); 
+        }
+
+        static void ReadBinaryFile(List<Coordinates> coordinates)
+        {
+
+            using (BinaryReader b = new BinaryReader(
+            File.Open("../../../Resources/VehiclePositions.dat", FileMode.Open)))
+            {
+                while (b.BaseStream.Position != b.BaseStream.Length)
+                {
+                    try
+                    {
+                        Coordinates tag = new Coordinates();
+
+                        tag.PositionId = b.ReadInt32();
+                        tag.VehicleRegistration = b.ReadByte().ToString();
+
+                        tag.Latitude = BitConverter.ToSingle(b.ReadBytes(4));
+
+                        tag.Longitude = BitConverter.ToSingle(b.ReadBytes(4));
+                        tag.RecordedTimeUTC = b.ReadInt64();
+
+                        coordinates.Add(tag);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"an error occured: {ex.Message}");
+                    }
+
+
+                } 
+                
+            }
+        }
+    }
+}
